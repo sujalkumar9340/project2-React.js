490853485535a961462d9d908470cac4a45bcdb7
Sujal Kumar <110787167+sujalkumar9340@users.noreply.github.com>
2023-10-06 07:27:44 +0530

    javascript file in build file in react

---
+++ App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+
+import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+    <>
+   <Navbar  title="Text-transfrom"/>
+  <div className="container my-3">
+  <TextForm heading=" Enter the text to analyze"/>
+    
+     {/* <About/> */}
+    </div>    
+  
+    </>
+  );
+};
+
+export default App;
